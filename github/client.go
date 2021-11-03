497f15d77256d42becf7d8721a9a34b35bf799e3
Carl Camit <carlcamit0@gmail.com>
2021-11-03 15:31:16 +1100

    Check Github API Availability
    Create Method to retrieve Github API status for the health check
    endpoint

--- github/client.go
+++ github/client.go
@@ -4,6 +4,12 @@ import (
 	"net/http"
 )
 
+// Github API encourages making requests
+// with this Accept header
+//
+// https://docs.github.com/en/rest/overview/media-types#request-specific-version
+var HeaderAccept = "application/vnd.github.v3+json"
+
 // Client specifies the settings for
 // communicating with the API
 type Client struct {
@@ -20,4 +26,23 @@ func NewClient(hostURL string) *Client {
 		hostURL: hostURL,
 		client:  &http.Client{Transport: http.DefaultTransport},
 	}
+}
+
+// CheckStatus checks the availability of the
+// Github API so that the health endpoint
+// can respond appropriately
+func (c *Client) CheckStatus() (int, error) {
+	req, err := http.NewRequest(http.MethodGet, c.hostURL, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header.Set("Accept", HeaderAccept)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+
+	return res.StatusCode, nil
 }
